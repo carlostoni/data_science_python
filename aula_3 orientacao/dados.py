794215adef062a095f97946c87f34c8b9a77c8d9
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2024-06-22 09:14:26 -0300

    Atualizar o dados.py

--- aula_3 orientacao/dados.py
+++ aula_3 orientacao/dados.py
@@ -1,34 +1,63 @@
-class Dados:
+class EmpresaDados:
     def __init__(self):
-        self.dados = {}
-        self.inserir_dados_inicial()
+        self.empresa1 = [1000, 6000, 1200, 8000, 1400]
+        self.empresa2 = [5000, 4000, 3000, 2000, 7000]
+        self.empresa3 = [1200, 1300, 8000, 3000, 15000]
+        self.empresa4 = [1400, 1750, 2000, 4500, 5900, 7000]
+        self.empresa5 = [1400, 1750, 2000, 4500, 5900, 7000]
 
-    def inserir_dados_inicial(self):
-        self.dados['empresa1'] = [1000, 6000, 1200, 8000, 1400]
-        self.dados['empresa2'] = [5000, 4000, 3000, 2000, 7000]
-        self.dados['empresa3'] = [1200, 1300, 8000, 3000, 15000]
-        self.dados['empresa4'] = [1400, 1750, 2000, 4500, 5900, 7000]
-        self.dados['empresa5'] = [1400, 1750, 2000, 4500, 5900, 7000]
-
-    def adicionar_dado(self, empresa, dado):
-        if empresa in self.dados:
-            self.dados[empresa].append(dado)
+    def get_empresa_dados(self, empresa_num):
+        if empresa_num == 1:
+            return self.empresa1
+        elif empresa_num == 2:
+            return self.empresa2
+        elif empresa_num == 3:
+            return self.empresa3
+        elif empresa_num == 4:
+            return self.empresa4
+        elif empresa_num == 5:
+            return self.empresa5
         else:
-            self.dados[empresa] = [dado]
-
-    def obter_dados(self):
-        return self.dados
+            return None
 
 
-class Resolucao:
-    def __init__(self):
-        self.dados = Dados().obter_dados()
+from Moda import moda
+from Media import media
+from Mediana import mediana
+from Desvio import desvio
 
-    def mostrar_dados(self):
-        for empresa, valores in self.dados.items():
-            print(f"{empresa} = {valores}")
+def main():
+    empresas = EmpresaDados()  # Cria uma instância da classe EmpresaDados
+    escolha_empresa = int(input('Escolha a empresa (1-5): '))
+    dados = empresas.get_empresa_dados(escolha_empresa)  # Obtém os dados da empresa selecionada
 
-# Programa principal
+    if dados is None:
+        print('Empresa inválida')
+        return
+
+    escolha = input(f'''
+1- Moda
+2- Media
+3- Mediana
+4- Desvio
+5- Todos
+: ''')
+
+    if escolha == '1':
+        print('Moda:', moda(dados))
+    elif escolha == '2':
+        print('Média:', media(dados))
+    elif escolha == '3':
+        print('Mediana:', mediana(dados))
+    elif escolha == '4':
+        print('Desvio padrão:', desvio(dados))
+    elif escolha == '5':
+        print('Moda:', moda(dados))
+        print('Média:', media(dados))
+        print('Mediana:', mediana(dados))
+        print('Desvio padrão:', desvio(dados))
+    else:
+        print('Opção inválida')
+
 if __name__ == "__main__":
-    resolucao = Resolucao()
-    resolucao.mostrar_dados()
+    main()
