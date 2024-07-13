f52f34631ca879f29991a67061c7a55f1b1d792d
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2024-07-13 12:23:37 -0300

    Atualizar o teste.py

--- teste.py
+++ teste.py
@@ -1,25 +1,43 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# Carregamento dos dados
-dados = pd.read_csv('covid_19.csv')
+# Carrega os dados do arquivo CSV
+dados = pd.read_csv('/mnt/data/file-3JdQMvkMlN0Ewu9ubG7osj8X')
 
-# Preenchimento de valores ausentes
-dados['população'].fillna(0, inplace=True)
-dados['testes'].fillna(0, inplace=True)
+# Converte as colunas de interesse em listas
+spectral = dados['Spectral Class'].fillna(0).to_list()
+star_color = dados['Star color'].fillna(0).to_list()
 
-# Seleção de dados
-populacao = dados['população'].to_list()
-testes = dados['testes'].to_list()
+# Seleciona uma porção dos dados para visualização
+n_spectral = spectral[1:176]
+n_star_color = star_color[1:176]
 
-# Geração do gráfico de barras
-plt.figure(figsize=(10, 6))
-plt.bar(dados['continente'], populacao, label='População')
-plt.bar(dados['continente'], testes, label='Testes', bottom=populacao)
-plt.xlabel('Continente')
-plt.ylabel('Quantidade')
-plt.title('População e Testes de COVID-19 por Continente')
-plt.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+# Cria o gráfico de barras
+fig, ax = plt.subplots()
+bars = ax.bar(n_spectral, n_star_color)
+
+# Adiciona os valores no topo de cada barra
+for bar in bars:
+    height = bar.get_height()
+    ax.annotate('{}'.format(height),
+                xy=(bar.get_x() + bar.get_width() / 2, height),
+                xytext=(0, 3),  # 3 points vertical offset
+                textcoords="offset points",
+                ha='center', va='bottom')
+
+# Configura o Tkinter
+Tk().withdraw()
+janela = tk.Tk()
+janela.geometry("1920x1080")
+
+# Adiciona o gráfico ao canvas
+canvas = FigureCanvasTkAgg(fig, master=janela)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+# Inicia o loop principal do Tkinter
+janela.mainloop()
