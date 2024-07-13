fc0e283f6fe55a582e18b082e6f0d11b75146d6d
Carlos Eduardo Toni de Azevedo <80012970+carlostoni@users.noreply.github.com>
2024-07-13 14:05:33 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -50,3 +50,45 @@ canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
 # Inicia o loop principal do Tkinter
 janela.mainloop()
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# Carrega os dados do arquivo CSV
+dados = pd.read_csv('brasileirao.csv')
+
+
+# Converte as colunas de interesse em listas
+team = dados['team'].fillna(0).to_list()
+points = dados['points'].fillna(0).to_list()
+
+# Seleciona uma porção dos dados para visualização
+n_team = team[1:1000]
+n_points  = points [1:1000]
+
+# Cria o gráfico de barras
+fig, ax = plt.subplots()
+bars = ax.bar(n_team, n_points)
+
+def on_closing():
+    janela.quit()
+    janela.destroy()
+
+
+Tk().withdraw()
+janela = tk.Tk()
+janela.geometry("1920x1080")
+janela.protocol("WM_DELETE_WINDOW", on_closing)
+
+# Adiciona o gráfico ao canvas
+canvas = FigureCanvasTkAgg(fig, master=janela)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+# Inicia o loop principal do Tkinter
+janela.mainloop()
